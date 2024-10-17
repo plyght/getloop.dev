5465dc31ba7b5e81ca2ca3919f150abc2c1f74ea
plyght <81438024+plyght@users.noreply.github.com>
2024-10-17 10:22:10 -0400

    wowooooooo

--- src/navigation.ts
+++ src/navigation.ts
@@ -13,10 +13,7 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/TheMrKai77' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/MrKai77/Loop' },
   ],
   footNote: `
